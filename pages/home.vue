09703afd997f1ffda2a303a3f9fb5476aebe9832
Densanity <denniszixiang@gmail.com>
2026-03-07 18:31:42 +0100

    init

--- pages/home.vue
+++ pages/home.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Stratagema</h1>
+  <section class="bg-secondary">
+    <l-trefoil
+      size="40"
+      stroke="4"
+      stroke-length="0.15"
+      bg-opacity="0.1"
+      speed="1.4"
+      color="black"
+    ></l-trefoil>
+    <h1>Stratagema</h1>
+  </section>
 </template>
+
+<script setup>
+import { trefoil } from "ldrs";
+trefoil.register();
+</script>
