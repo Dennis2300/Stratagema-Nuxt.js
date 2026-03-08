8c4ad8f879226cc3b509d79822983b5eab3cb047
Densanity <denniszixiang@gmail.com>
2026-03-08 19:45:27 +0100

    change the favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.webp" }],
+    },
+  },
   vue: {
     compilerOptions: {
       isCustomElement: (tag) => tag.startsWith("l-"),
