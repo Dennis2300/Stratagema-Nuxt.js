f3e8bf21937d3d9628772df3c2c043867611586c
Densanity <denniszixiang@gmail.com>
2026-03-10 18:15:39 +0100

    Update index.vue

--- pages/characters/index.vue
+++ pages/characters/index.vue
@@ -47,7 +47,7 @@
               d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"
             />
           </svg>
-          <span>Filters</span>
+          <p class="cursor-pointer">Filters</p>
         </div>
       </div>
       <div class="divider px-32 mt-0 mb-8"></div>
@@ -59,7 +59,7 @@
         <div
           v-for="character in characters"
           :key="character?.name"
-          class="relative bg-app-tertiary w-2/3 overflow-hidden rounded-xl border border-white/5 transition-transform duration-200 hover:translate-x-2"
+          class="relative bg-app-tertiary w-2/3 overflow-hidden rounded-xl border border-white/5 transition-transform duration-200 hover:translate-x-3"
         >
           <NuxtLink
             :to="`/characters/${character.id}?name=${encodeURIComponent(
