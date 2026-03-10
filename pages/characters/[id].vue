e51a51d3f728db9b5707af2d38a4e008c9898b5b
Densanity <denniszixiang@gmail.com>
2026-03-10 04:20:23 +0100

    added character info

--- pages/characters/[id].vue
+++ pages/characters/[id].vue
@@ -5,7 +5,8 @@
       <div class="flex justify-center">
         <img class="opacity-15" :src="character.splash_art_url" alt="" />
       </div>
-      <section class="absolute top-0 left-0 w-full px-32 py-10">
+      <section class="absolute top-0 left-0 w-full px-32 py-12 space-y-8">
+        <!--Avatar, Tags & Voice Actors-->
         <div class="flex justify-between items-center">
           <!--Avatar & Tags-->
           <div class="flex items-center gap-4">
@@ -31,7 +32,7 @@
             </div>
             <div>
               <h2 class="font-acme">{{ character.name }}</h2>
-              <div class="space-x-4">
+              <div class="space-x-2">
                 <span class="badge badge-primary">{{ character.role }}</span>
                 <span class="badge badge-primary">{{
                   character.vision.name
@@ -45,7 +46,7 @@
               </div>
             </div>
           </div>
-          <!-- Voice Actors -->
+          <!--Voice Actors-->
           <div class="space-y-2">
             <div>
               <h5 class="uppercase tracking-wider text-white/60 leading-none">
@@ -78,6 +79,77 @@
                 </span>
               </div>
             </div>
+          </div>
+        </div>
+        <!--Character Info-->
+        <div class="grid grid-cols-2 gap-x-24 gap-y-4">
+          <!--Rarity-->
+          <div
+            class="flex justify-between text-2xl font-acme bg-white/5 border border-white/5 rounded-lg px-4 py-2"
+          >
+            <span class="text-app-muted">Rarity</span>
+            <div>
+              <span
+                v-for="n in character.rarity"
+                :key="n"
+                class="text-2xl leading-none"
+                :class="
+                  character.rarity === 5 ? 'text-amber-400' : 'text-purple-400'
+                "
+                >★</span
+              >
+            </div>
+          </div>
+          <!--Cons-->
+          <div
+            class="flex justify-between text-2xl font-acme bg-white/5 border border-white/5 rounded-lg px-4 py-2"
+          >
+            <span class="text-app-muted">Constellation</span>
+            <span>{{ character.constellation }}</span>
+          </div>
+          <!--Birthday-->
+          <div
+            class="flex justify-between text-2xl font-acme bg-white/5 border border-white/5 rounded-lg px-4 py-2"
+          >
+            <span class="text-app-muted">Birthday</span>
+            <span>{{ character.birthday }}</span>
+          </div>
+          <!--Weapon Type-->
+          <div
+            class="flex justify-between text-2xl font-acme bg-white/5 border border-white/5 rounded-lg px-4 py-2"
+          >
+            <span class="text-app-muted">Weapon Type</span>
+            <div class="flex gap-2 items-center">
+              <span>{{ character.weapon_type.name }}</span>
+              <img class="w-8" :src="character.weapon_type.img_url" alt="" />
+            </div>
+          </div>
+          <!--Signature Dish-->
+          <div
+            class="flex justify-between text-2xl font-acme bg-white/5 border border-white/5 rounded-lg px-4 py-2"
+          >
+            <span class="text-app-muted">Signature Dish</span>
+            <span>{{ character.signature_dish.name }}</span>
+          </div>
+          <!--Release Date-->
+          <div
+            class="flex justify-between text-2xl font-acme bg-white/5 border border-white/5 rounded-lg px-4 py-2"
+          >
+            <span class="text-app-muted">Release Date</span>
+            <span>
+              {{
+                character.release_date
+                  ? new Date(character.release_date).toLocaleDateString(
+                      "en-US",
+                      {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      },
+                    )
+                  : "UPCOMING"
+              }}
+            </span>
           </div>
         </div>
       </section>
