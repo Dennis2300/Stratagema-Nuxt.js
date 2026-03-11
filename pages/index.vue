5163a70e518bf6cc78ed07de2345fd2f49529e42
Densanity <denniszixiang@gmail.com>
2026-03-11 16:36:41 +0100

    moved home.vue to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,51 @@
-<script setup lang="ts">
-import Home from "./home.vue";
-</script>
-
 <template>
-  <Home />
+  <main class="flex flex-col items-center">
+    <!--Loading-->
+    <section v-if="loading">
+      <div class="flex justify-center items-center h-64">
+        <LoadingSpinner />
+      </div>
+    </section>
+    <!--Error-->
+    <section v-else-if="error">
+      <ErrorMessage :error="error" />
+    </section>
+    <!--Content-->
+    <section
+      v-else
+      class="flex flex-col justify-center items-center space-y-24 mt-10"
+    >
+      <!--Navigation Cards-->
+      <HomePageNavigation />
+      <!-- Current Banner characters-->
+      <CurrentBanner />
+      <!--Notice Boards-->
+      <NoticeBoard />
+    </section>
+  </main>
 </template>
+
+<script setup>
+const supabase = useSupabaseClient();
+const loading = ref(null);
+const error = ref(null);
+const updates = ref([]);
+
+async function getAllUpdates() {
+  loading.value = true;
+  try {
+    const { data, err } = await supabase.from("updates").select("*");
+    if (err) throw err;
+    updates.value = data;
+  } catch (e) {
+    error.value = e;
+    console.error(e);
+  } finally {
+    loading.value = false;
+  }
+}
+
+onMounted(() => {
+  getAllUpdates();
+});
+</script>
