793427483f055330b9e92c02d542e0329cbc3c70
Densanity <denniszixiang@gmail.com>
2026-03-12 17:27:35 +0100

    added height so the img is centered

--- components/NoCharacterFound.vue
+++ components/NoCharacterFound.vue
@@ -1,6 +1,7 @@
 <template>
-  <article class="flex flex-col justify-center items-center">
+  <article class="flex flex-col items-center min-h-[70vh]">
     <img
+      class="w-32"
       src="https://act-upload.hoyoverse.com/event-ugc-hoyowiki/2025/03/03/151578876/d20b6b813decbe3db21748c266ec9f93_8099285462854447444.png?x-oss-process=image%2Fformat%2Cwebp"
       loading="lazy"
       alt=""
